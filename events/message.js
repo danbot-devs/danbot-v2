947258395b3dff9803090e8e37e44a56aa68ebf2
danielpmc <danielpd93@gmail.com>
2019-10-13 00:18:53 +0100

    Updated console logs to display guild and user id's

--- events/message.js
+++ events/message.js
@@ -19,7 +19,7 @@ module.exports = async (client, message, guild) => {
           const args = message.content.slice(prefix.length).trim().split(/ +/g);
           const commandargs = message.content.split(' ').slice(1).join(' ');
           const command = args.shift().toLowerCase();
-          client.channels.get(config.consoleLogs).send(`[${message.guild.name}] [${message.author.username}] >> ${settings.prefix}${command} ${commandargs}`);
+          client.channels.get(config.consoleLogs).send(`[${message.guild.name} (${message.guild.id})] [${message.author.username} (${message.author.id})] >> ${settings.prefix}${command} ${commandargs}`);
               try {
                   let commandFile = require(`../commands/${command}.js`);
                   commandFile.run(client, message, args);
