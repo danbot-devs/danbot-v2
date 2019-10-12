948c77c936d08f3f655a7bf0bb0f50ef701f1cc0
Hadimhz <40409775+Hadimhz@users.noreply.github.com>
2019-10-12 18:56:34 +0300

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,20 @@
+const config = {
+    "ownerID": "BotOwner",
+
+    "token": "BotToken",
+
+    "GoogleAPIKey": "GoogleAPIKey",
+
+    "consoleLogs": "ConsoleLogsChannelID",
+    "startupLogs": "StartUpLogsChannelID",
+  
+    "dashboard" : {
+        "oauthSecret": "OauthSecrethere",
+        "callbackURL": "https://example.example/callback",
+        "sessionSecret": "enterasecrethere",
+        "domain": "https://example.example",
+        "port": PORT
+      },
+  };
+  
+  module.exports = config;
