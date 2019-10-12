27dbc205dca790ccde27d6a3bccfe150b7ec4fb9
Hadimhz <40409775+Hadimhz@users.noreply.github.com>
2019-10-12 17:56:48 +0300

    Command handler 1/2
    Started working on the Command handler!

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //Consts that make the bot work!
 global.Discord = require("discord.js");
 const client = Discord.Client();
+const bot = client
 global.fs = require("fs");
 global.moment = require("moment");
 global.SQLite = require("sqlite3")
@@ -10,17 +11,27 @@ global.ms = require('ms')
 global.request = require('request')
 
 
-//Event handler
-fs.readdir('./events/', (err, files) => {
-    files = files.filter(f => f.endsWith('.js'));
-    files.forEach(f => {
-        const event = require(`./events/${f}`);
-        client.on(f.split('.')[0], event.bind(null, client));
-        delete require.cache[require.resolve(`./events/${f}`)];
+
+client.on('ready', () => {
+  
+  //Event handler
+  fs.readdir('./events/', (err, files) => {
+      files = files.filter(f => f.endsWith('.js'));
+      files.forEach(f => {
+          const event = require(`./events/${f}`);
+          client.on(f.split('.')[0], event.bind(null, client));
+          delete require.cache[require.resolve(`./events/${f}`)];
+        });
       });
-    });
+  
+  //Command handler
 
 
+})
+
+client.on('message', async message => {
+
+})
 
 
 //Allow the bot to log into discord
